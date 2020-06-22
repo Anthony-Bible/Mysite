e9be8c150ecdb36f58d43f27c63e1905f7c636ba
anthony bible <anthony@anthonybible.com>
2020-06-21 20:13:09 -0600

    removed the extra alert

--- index.php
+++ index.php
@@ -112,12 +112,6 @@
         </div>
         <!-- /.container-fluid -->
     </nav>
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-   You should check my <a href="https://github.com/anthony-bible" class="alert-link">github </a> to see some of the latest projects I'm working on. Or check out my <a href="www.abible.dev" class="alert-link"> blog </a> for a more detailed look into what I do. 
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
     <!-- Header -->
     <header>
         <div class="container" id="maincontent" tabindex="-1">
